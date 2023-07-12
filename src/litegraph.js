7668fb26b649372dd978eb07abfbfc8ea79c3287
Will <wilt00@users.noreply.github.com>
2023-07-12 12:02:07 -0700

    Support arrow function in wrapFunctionAsNode

--- src/litegraph.js
+++ src/litegraph.js
@@ -14160,6 +14160,7 @@ LGraphNode.prototype.executeAction = function(action)
             .replace(/\s+/g, "") // strip white space
             .replace(/[/][*][^/*]*[*][/]/g, "") // strip multi-line comments  /**/
             .split("){", 1)[0]
+            .split(")=>", 1)[0]
             .replace(/^[^(]*[(]/, "") // extract the parameters
             .replace(/=[^,]+/g, "") // strip any ES6 defaults
             .split(",")
