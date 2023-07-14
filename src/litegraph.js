3dd7af320a019120e12f2a55a03437d4624086f5
Test User <will.toher.0@gmail.com>
2023-07-14 13:05:43 -0700

    Support arrow function in wrapFunctionAsNode

--- src/litegraph.js
+++ src/litegraph.js
@@ -6154,10 +6154,10 @@ LGraphNode.prototype.executeAction = function(action)
                             e.preventDefault();
                             e.stopPropagation();
                         } else if (this.allow_searchbox) {
-						this.showSearchBox(e);
-						e.preventDefault();
-						e.stopPropagation();
-					}
+                            this.showSearchBox(e);
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }
                     }
 
 					clicking_canvas_bg = true;
@@ -14165,7 +14165,7 @@ LGraphNode.prototype.executeAction = function(action)
             .replace(/[/][/].*$/gm, "") // strip single-line comments
             .replace(/\s+/g, "") // strip white space
             .replace(/[/][*][^/*]*[*][/]/g, "") // strip multi-line comments  /**/
-            .split("){", 1)[0]
+            .split(/\)?(\{|=>)/, 1)[0]
             .replace(/^[^(]*[(]/, "") // extract the parameters
             .replace(/=[^,]+/g, "") // strip any ES6 defaults
             .split(",")
